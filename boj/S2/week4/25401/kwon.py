ae9a19467f8652b88caf0e835cee72b15d707082
Mano <qja1998@naver.com>
2025-10-12 00:08:30 +0900

    solved: [BOJ_25401] 카드 바꾸기

---
+++ boj/S2/week4/25401/kwon.py
@@ -0,0 +1,27 @@
+# BOJ 25401 카드 바꾸기
+
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+cards = list(map(int, input().split()))
+
+ans = n - 2  # 최대 교체 횟수는 n-2
+
+# 모든 가능한 두 카드 조합 (i, j)에 대해
+for i in range(n):
+    for j in range(i + 1, n):
+        if (cards[j] - cards[i]) % (j - i) != 0:
+            continue
+        d = (cards[j] - cards[i]) // (j - i)
+        cnt = 0
+        
+        # 각 위치에서 예상값과 실제값 비교
+        for k in range(n):
+            expected = cards[i] + (k - i) * d
+            if cards[k] != expected:
+                cnt += 1
+        
+        ans = min(ans, cnt)
+
+print(ans)
